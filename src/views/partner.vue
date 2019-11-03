97004d3a7901cb773e473df1f26de932260b201b
cyy <1689456277@qq.com>
2019-11-03 11:54:58 +0800

    partner over

--- src/views/partner.vue
+++ src/views/partner.vue
@@ -1,13 +1,52 @@
 <template>
-    
+    <div class="partner-page">
+        <banner :bannerInfo=bannerInfo />
+        <bread-nav />
+        <div class="p-tit">合作伙伴</div>
+        <div class="list-group clearfix">
+            <div class="list" v-for="item,index in list" :key="">
+                <img :src="item.src" class="full" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            bannerInfo: {
+                title: '合作伙伴',
+                desc: '诚信合作 互惠共赢 稳步发展 ',
+                url: 'static/contact-banner.png'
+            },
+            list: [
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+                { src: 'static/cooperation-logo.jpg', },
+            ]
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.partner-page{
+    .list-group{
+        calcmedia('mt', 60px);
+        .list{
+            calcmedia('w', calc(16.6% - 30px), calc(50% - 30px));
+            calcmedia('m', 0 15px 20px); float: left; border: 1px solid $border;
+        }
+    }
+}
 </style>
